57b8588ad5ac33497f3fb48860ad37d6c400a609
julesbruins <jf-bruins@hotmail.com>
2025-09-16 11:58:14 +0200

    started flipping card #9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,10 @@
 <main>
     <h1>{ memberMe.name }</h1>
     <p>Get to know better by putting your mouse over the hearts on the card! Curious about the face behind the little fun facts? Flip the card.</p>
-    
-    <section class="poker-card-info">
 
-        <div class="icon-top"></div> 
+<div class="flipping">
+    <section class="poker-card-info">
+        <span class="icon-top">♥️</span>
             <ul>
                 <li>
                     <button class="icon">♥️</button>
@@ -38,8 +38,25 @@
                     <button class="title">Fav coffee: { memberMe.fav_coffee }</button>
                 </li>
             </ul>
-        <div class="icon-bottom"></div> 
+        <span class="icon-bottom">♥️</span>
     </section>
+
+
+    <section class="poker-card-photo">
+        <span class="icon-top">♥️</span>
+            <h2>{ memberMe.name }</h2>
+            <img src="https://fdnd.directus.app/assets/{memberMe.mugshot}" alt="">
+        <span class="icon-bottom">♥️</span>
+    </section>
+
+    <form>
+        <input type="radio" id="front" name="profile-card" value="front"/>
+        <label for="front">front</label><br />
+
+        <input type="radio" id="back" name="profile-card" value="back" />
+        <label for="back">back</label><br/>
+    </form>
+</div>  
 
 </main>
 
@@ -83,7 +100,6 @@
         --border: 1px solid var(--card-accent-dark);
 
         background-color: green;
-        padding: 1rem;
         width: 100vw;
         height: 100vh;
     }
@@ -114,7 +130,7 @@
         font-size: 1rem;
     }
 
-    .poker-card-info {
+    /* section {
         background-color: var(--background-color-two);
         border: var(--border);
         border-radius: var(--border-radius);
@@ -129,10 +145,23 @@
         place-items: center;
         position: relative;
         margin: 0 auto;
+    } */
+    /* .poker-card-photo {
+        gap: 1rem;
+        justify-content: center;
+        
+        h2 {
+            font-family: var(--secondary-font);
+            color: var(--card-accent-dark);
+        }
+        img {
+            width: 100%;
+            border-radius: var(--border-radius);
+            border: var(--border);
+        }
+    } */
 
-    }
-
-    .icon-top:before,
+    /* .icon-top:before,
     .icon-bottom:after {
         content: var(--heart);
         background-color: var(--background-color-two);
@@ -150,13 +179,58 @@
         bottom: 0;
         right: 0;
         rotate: 180deg;
+    } */
+
+    
+    section {
+        position: absolute;
+        background-color: var(--background-color-two);
+        border: var(--border);
+        border-radius: var(--border-radius);
+
+        aspect-ratio: 5 / 7;
+        width: 100%;
+        max-width: 360px;
+        
+        left: 50%;
+        transform: translateX(-50%);
+
+
+        .icon-top {
+            position: absolute;
+            top: 1rem;
+            left: 1rem;
+        }
+        .icon-bottom {
+            position: absolute;
+            bottom: 1rem;
+            right: 1rem;
+            rotate: 180deg;
+        }
+
     }
-    ::before::before {
-        content: var(--number);
+
+    .poker-card-photo {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: center;
+        justify-content: center;
+        z-index: -1;
+        
+        h2 {
+            font-family: var(--secondary-font);
+            color: var(--card-accent-dark);
+        }
+        img {
+            width: 80%;
+            border-radius: var(--border-radius);
+            border: var(--border);
+        }
     }
 
 
-    /* HOVER ANIMATION */
+    /* HOVER ANIMATION HEARTS */
     ul {
         display: grid;
         grid-template-columns: 1fr 1fr;
@@ -164,7 +238,9 @@
         position: relative; 
         cursor: pointer;
         height: 100%;
-        align-items: center;
+        place-content: center;
+        width: fit-content;
+        margin: 0 auto;
         
         li {
             position: relative;
@@ -209,6 +285,24 @@
     ul li:hover .title {
         transform: scale(1);
         transition-delay: 0.25s;
+    }
+
+    /* FLIPPING CARD */
+    form {
+        display: flex;
+        gap: 1rem;
+        position: absolute;
+        bottom: 1rem;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    div:has([value="back"]:checked) .poker-card-photo {
+        z-index: 1;
+        /* transform: rotateY(-180deg);       */
+    }
+    div:has([value="back"]:checked) .poker-card-info {
+        /* transform: rotateY(-180deg); */
     }
 
 
